79a91934571d12dc2efed6e91b7b37bb12bf5def
Keith Reynolds <strange.aspiration@gmail.com>
2015-03-12 21:38:13 +0000

    Returns weather, temp, icon

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,23 +1,48 @@
-$(document).ready(function() {
-
+$(document).ready(function(){
+	
+	// var weather = "http://api.wunderground.com/api/ea66dc7b777b2081/geolookup/conditions/forecast/"
+	var cities;
 	var city = $('#city');
 	var matchedCities = function(response){
-		var cityP = ('<p>')
-		$.each(response, function(i,value) {
-			cityP.text(response[i].name);
-			$('.list').append(cityP);
-		});
+		cities = response.RESULTS;
+		for (i=0, leng = cities.length; i<leng; i++) {
+			$('.list').append('<p><a href="http://api.wunderground.com/api/ea66dc7b777b2081/geolookup/conditions/forecast/' + cities[i].l + '.json">' + cities[i].name + '</a></p>');
+		};
+	};
 
-	}
+	var cityWeather = function(data){
+		var newWeather = {}
+		newWeather.weather = data.current_observation.weather;
+		newWeather.temp_c = data.current_observation.temp_c;
+		newWeather.icon_url = data.current_observation.icon_url;
+		console.log(newWeather);
+		$('.weather').append('<p>' + newWeather.weather + ' ' + newWeather.temp_c +'</p>' + '<img src=' + newWeather.icon_url + '>')
+	};
 
 	$('.submit').on('click', function(evt){
 		evt.preventDefault();
 		$.ajax({
 			method: "GET",
-			url: "http://autocomplete.wunderground.com/aq?query=" + city.val() + '%',
-			dataType: 'json',
+			url: "http://autocomplete.wunderground.com/aq?query=" + city.val(),
+			dataType: 'jsonp',
+			jsonp: 'cb',
 			success: matchedCities
 		})
 	});
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+
+	$('.list').on('click','p a', function(evt){
+		evt.preventDefault();
+		$.ajax({
+			method: "GET",
+			url: this,
+			dataType: 'json',
+			jsonp: 'cb',
+			success: cityWeather
+		})
+
+
+		
+	});
+
 });
