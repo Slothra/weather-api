5a8150a867f86f09dc4904fae79884d278fe71a8
Keith Reynolds <strange.aspiration@gmail.com>
2015-03-13 05:30:38 +0000

    Adds css

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -5,18 +5,24 @@ $(document).ready(function(){
 	var city = $('#city');
 	var matchedCities = function(response){
 		cities = response.RESULTS;
-		for (i=0, leng = cities.length; i<leng; i++) {
+		for (i=0; i<10; i++) {
 			$('.list').append('<p><a href="http://api.wunderground.com/api/ea66dc7b777b2081/geolookup/conditions/forecast/' + cities[i].l + '.json">' + cities[i].name + '</a></p>');
-		};
-	};
+			$('.list').addClass('is-visible');
+		}
+	}
 
 	var cityWeather = function(data){
 		var newWeather = {}
+		newWeather.name = data.current_observation.display_location.city;
 		newWeather.weather = data.current_observation.weather;
 		newWeather.temp_c = data.current_observation.temp_c;
 		newWeather.icon_url = data.current_observation.icon_url;
-		console.log(newWeather);
-		$('.weather').append('<p>' + newWeather.weather + ' ' + newWeather.temp_c +'</p>' + '<img src=' + newWeather.icon_url + '>')
+		newWeather.lat = data.current_observation.display_location.latitude;
+		newWeather.long = data.current_observation.display_location.longitude;
+		$('.weather').append('<h2>' + newWeather.name + '<h2><p>' + newWeather.weather + ' ' + newWeather.temp_c +' C <br/><br/><img src=' + newWeather.icon_url + '></p>')
+		$('.list').removeClass('is-visible');
+		$('.list').hide(100);
+		$('.weather').addClass('is-visible');
 	};
 
 	$('.submit').on('click', function(evt){
@@ -40,8 +46,6 @@ $(document).ready(function(){
 			jsonp: 'cb',
 			success: cityWeather
 		})
-
-
 		
 	});
 
